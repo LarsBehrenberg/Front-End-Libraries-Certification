d2d81f8cc60b7441e3390353f83f9fc3fc9f0175
Lars Behrenberg <32539217+LarsBehrenberg@users.noreply.github.com>
2019-07-14 19:34:50 +0900

    Cleaning up and commenting code

--- 02-markdown-previewer/src/MarkdownPreviewer.js
+++ 02-markdown-previewer/src/MarkdownPreviewer.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-multi-str */
 import React from 'react';
-import './App.css';
+import './MarkdownPreviewer.css';
 import marked from 'marked';
 
 // Set options for marked -> set breaks = true for freeCodeCamp test number 7 on this challenge
@@ -15,71 +15,40 @@ marked.setOptions({
   xhtml: false
 });
 
-
+// Initial text displayed in the #editor input field
 var text = "# Welcome to my React Markdown Previewer!\n\n## Sub header\n\n Heres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n }\n}\n```\n\nYou can also make text **bold**... whoa!\n\nThere's also [links](https://www.freecodecamp.com), and\n> 'Block Quotes!' by somebody\n\n1. And there are numbererd lists too.\n1. Use just 1s if you want! \n1. But the list goes on...!\n\n![React Logo w/ Text](https://goo.gl/Umyytc)"
 
-class App extends React.Component {
+// Main class later exported to #root div
+export default class MarkdownPreviewer extends React.Component {
   constructor(props){
     super(props);
 
     this.state = {
-      input: text,
+      input: text, // setting intitial state as previously defined text
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChange = this.handleChange.bind(this); //binding handleChange function
   }
 
+  // Called onChange in the textarea, sets new state.input
   handleChange(event) {
     this.setState({
       input: event.target.value
     })
-    console.log(this.state.input);
   }
 
+  // function of marked -> returning the state.input as html to display in the #preview div
   getMarkdownText() {
-    var rawMarkup = marked(this.state.input, {sanitize: true});
-    return { __html: rawMarkup};
+    return { __html: marked(this.state.input, {sanitize: true})};
   }
 
   render(){
     return (
-      <div className="App">
-        <h1 style={{color: "white"}}>Markdown Previewer</h1>
-        <textarea id="editor" value={this.state.input} onChange={this.handleChange} autofocus name="# Default" >{this.state.input}</textarea>
+      <div className="MarkdownPreviewer">
+        <textarea id="editor" value={this.state.input} onChange={this.handleChange} autoFocus name="# Default" >{this.state.input}</textarea>
         <div id="preview" dangerouslySetInnerHTML={this.getMarkdownText()} />
       </div>
     );
   }
   
 }
-
-export default App;
-
-
-
-
- 
-// ----------  OLD CODE !!!!  ----------
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
