e5f788e189fe0688b69cf335818d38a75de6f719
Lars Behrenberg <32539217+LarsBehrenberg@users.noreply.github.com>
2019-09-08 12:16:48 +0900

    working version 1.0

--- 04-calculator/src/index.js
+++ 04-calculator/src/index.js
@@ -1,95 +1,54 @@
-var currentNumber = 0;
-var operator = "";
+var currentNumber = 0
 var result = 0;
-var display = true;
-var equalIsPressed = false;
+var display = true
+var equalPressed = false;
 
-// Get number function
+// Display number in display div - keyButtons (0-9)
 function displayCurrentNumber(keyData) {
-    if (display || document.getElementById("display").innerHTML == "0") {
+    if (display || document.getElementById("display").innerText == "0") { // If newNumber = true then replace number in display or if number = 0 then also replace
+        document.getElementById("display").innerText = keyData; // Display number in display
+        equalPressed ? document.getElementById("calculation-display").innerText = keyData : document.getElementById("calculation-display").innerText += keyData;
+        equalPressed = false;
         display = false;
-        document.getElementById("display").innerHTML = keyData
-        currentNumber = Number(document.getElementById("display").innerHTML);
-        console.log("currentNumber: " + currentNumber); // log it
-    } else {
-        if (document.getElementById("display").innerHTML.length < 10) {
-            document.getElementById("display").innerHTML += keyData
-            currentNumber = Number(document.getElementById("display").innerHTML);
-            console.log("currentNumber: " + currentNumber); // log it
-        } else {
-            console.log("too long")
-        }
+    } else if (document.getElementById("display").innerText.length < 10) { // Prevent numbers from being too long && add to current number instead of repleacing number in display
+        document.getElementById("display").innerText += keyData;
+        document.getElementById("calculation-display").innerText += keyData;
     }
-
 }
 
-
+// Add dot to number button
 function dot() {
-    if (!document.getElementById("display").innerHTML.includes(".")) {
+    if (!document.getElementById("display").innerHTML.includes(".")) { // if dot not include in display yet, then add to number
         display = false;
         document.getElementById("display").innerHTML += ".";
-        currentNumber = Number(document.getElementById("display").innerHTML); // store new displayed number in currentNumber
-        console.log("currentNumber: " + currentNumber); // log it
-    } else {
-        console.log("dot is already there");
+        document.getElementById("calculation-display").innerText += ".";
     }
 }
 
-
+// operator (+, -, *, /) function
 function operatorPressed(currentOperator) {
-
-        if (operator == "" && !equalIsPressed) {
-            result = currentNumber; // Store currentNumber
-            operator = currentOperator;
-        } else if (equalIsPressed) {
-            equalIsPressed = false;
-            operator = currentOperator;
-        } else {
-            equal();
-            operator = currentOperator;
-        }
-        display = true; // Enter new number
-
-    
-
+    document.getElementById("display").innerText = currentOperator;
+    document.getElementById("calculation-display").innerText += currentOperator; // Add operator to current calculation
+    display = true; // new number has to be entered = true
+    equalPressed = false; // equal is not pressed last
+    document.getElementById("calculation-display").innerText = document.getElementById("calculation-display").innerText.replace(/(\D)\1/, currentOperator) // To prevent doubling the same operator
+    document.getElementById("calculation-display").innerText = document.getElementById("calculation-display").innerText.replace(/\D{3}/, currentOperator) // Prevent from entering operator more than 2 at a time after another
 }
 
-function equal(isPressed) {
-        switch (operator){
-            case "divide":
-                    console.log("result div");
-                    result /= currentNumber;
-                    break;
-                case "multiply":
-                    console.log("result multiply");
-                    result *= currentNumber;
-                    break;
-                case "subtract":
-                    console.log("result subtract");
-                    result -= currentNumber;
-                    break;
-                case "addition":
-                    console.log("result addition");
-                    result += currentNumber;
-                    break;
-                default: 
-                    break;
-        }
-
-        if(isPressed){
-            operator= "";
-            equalIsPressed = true;
-        }
-    
-    document.getElementById("display").innerHTML = result;
-    display = true; // Enter new number
-}
-
-function ac() {
-    currentNumber = 0;
-    operator = "";
-    result = 0;
-    equalIsPressed = false;
+// (=)Button function
+function equal() {
+    if (document.getElementById("calculation-display").innerHTML != "") { // If calculation is not empty (calculationdisplay is not empty)
+        result = eval(document.getElementById("calculation-display").innerHTML); // Store calculation result in var result
+        document.getElementById("display").innerText = result; // Display result in display
+        document.getElementById("calculation-display").innerText = result; // Display result in calculation-display
+    }
+    equalPressed = true;  // equalPressed is important, so that if newNumber is put in it resets properly
     display = true;
-    document.getElementById("display").innerHTML = currentNumber;
+}
+
+// AC button function - Resetting the calculator at any point to its original state
+function ac() {
+    display = true;
+    document.getElementById("display").innerText = 0;
+    document.getElementById("calculation-display").innerText = "";
 }
